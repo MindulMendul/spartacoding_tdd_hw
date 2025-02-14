d2637a70e6c3e94abccea5f485299cebae09d1bd
Mindul <pencake33@naver.com>
2025-02-14 21:28:05 +0900

    week2 빈번하게 등장하는 단일문자 찾기 함수 TDD 구현

--- src/utils/week2.ts
+++ src/utils/week2.ts
@@ -1,16 +1,30 @@
-export const findMostFrequentChar=(str: string)=>{
-  const radix=new Map();
-  radix.set('a',0); radix.set('b',0); radix.set('c',0); radix.set('d',0); radix.set('e',0);
-  radix.set('f',0); radix.set('g',0); radix.set('h',0); radix.set('i',0); radix.set('j',0);
-  radix.set('k',0); radix.set('l',0); radix.set('m',0); radix.set('n',0); radix.set('o',0);
-  radix.set('p',0); radix.set('q',0); radix.set('r',0); radix.set('s',0); radix.set('t',0);
-  radix.set('u',0); radix.set('v',0); radix.set('w',0); radix.set('x',0); radix.set('y',0);
-  radix.set('z',0);
-  
-  str.split("").map((e)=>{
-    const c=e.toLowerCase();
-    radix.set(c, radix.get(c)+1);
+// 문제에 추가 조건을 달자!
+// => 공란은 단일문자라고 취급하지 않을 예정
+
+// 테스트 입력은 string으로 받을 예정
+// 테스트 예상 결과는 Array에 정답이 될 수 있는 모든 단일문자를 담아서 반환함
+// 만약, 공백을 제외한 문자열의 길이가 0이라면 [] 을 반환함
+
+export const findMostFrequentChar: (str: string) => Array<string> = (
+  str: string
+) => {
+  const radix = new Map(); // 각각의 단일 문자 개수 세는 map
+  const arr = str.replace(/\s/gi, '').split(''); // 공백 제거
+  if (arr.length == 0) return []; // 공백을 제외한 문자열의 길이가 0일 때는 [] 을 반환
+
+  // 열심히 세는 중
+  arr.forEach((c) => {
+    if (!radix.get(c)) radix.set(c, 0);
+    radix.set(c, radix.get(c) + 1);
   });
 
+  // 최대값 찾기
+  let _max = -1;
+  radix.forEach((v) => {
+    if (_max < v) _max = v;
+  });
 
-}
+  // 최대값이 같은 애들만 radix에서 후보를 고르고, 그 key 값만 정답으로 취함
+  // 이렇게 하면 시간복잡도는 O(str.length), 공간복잡도 역시 O(str.length) 가 됨!
+  return [...radix].filter((e) => e[1] == _max).map((e) => e[0]);
+};
